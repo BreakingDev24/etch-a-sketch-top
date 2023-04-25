b19e91f2497e02f011073d8a6d8e650fd2e71630
BreakingDev24 <antonio.avolio12@gmail.com>
2023-04-25 17:05:05 +0200

    create createGrid function

---
+++ script.js
@@ -0,0 +1,38 @@
+const pixelContainer = document.getElementById('pixel-container');
+const changeGridSizeBtn = document.getElementById('change-grid-size-btn')
+const pixelElement = document.querySelectorAll('pixel')
+
+let gridSize = 16;
+
+changeGridSizeBtn.addEventListener('click', updateGrid)
+
+function createGrid(size) {
+    pixelContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    pixelContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+    for (let i = 0; i < size * size; i++){
+        const pixels = document.createElement('div');
+        pixels.classList.add('pixel');
+        pixelContainer.appendChild(pixels)
+    }
+}
+
+createGrid(gridSize)
+
+function updateGrid () {
+    let newGrid = prompt('enter a number')
+    gridSize = parseInt(newGrid)
+    removeOldGrid()
+    createGrid(gridSize)
+
+}
+
+function removeOldGrid (){
+    while (pixelContainer.lastElementChild){
+        pixelContainer.removeChild(pixelContainer.lastElementChild)
+    }
+}
+
+
+
+console.log(pixelElement);
